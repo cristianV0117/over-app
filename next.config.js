2a388ef8087cd2525b54bdad848e6c41db58745e
Cristian Vasquez <cvo6372@gmail.com>
2021-08-21 21:31:44 -0500

    configuracion de modulo exports
    configuracion de modulo exports

--- next.config.js
+++ next.config.js
@@ -2,4 +2,9 @@ const withFonts = require('next-fonts');
 const withImages = require('next-images');
 const withPlugins = require('next-compose-plugins');
 
-module.exports = withPlugins([withFonts, withImages]);
+module.exports = withPlugins([withFonts, withImages], {
+	env: {
+		API: process.env.API,
+		DOMAIN_API: process.env.DOMAIN_API
+	}
+});
